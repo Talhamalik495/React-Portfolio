89dba18e627a5b5ddd98f992de1ffcf32194f77f
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-10-27 02:54:11 +0500

    home section add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   return (
     <>
-      <div className="w-[100%] ml-24 bg-[#111111] h-20">
-        <div></div>
-        <div></div>
+      <div className="w-[92%] ml-24 bg-[#111111] h-20 text-white flex justify-around items-center">
+        <div className="flex gap-5">
+          <FaFacebookF />
+          <FaTwitter />
+          <FaInstagram />
+          <FaLinkedinIn />
+        </div>
+        <div>
+          <h1>@2024copyright all right reserved</h1>
+        </div>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-
+import Boy from "../assets/boy.png";
 function Home() {
   return (
     <>
-      <h1>Home page ha ya</h1>
+      <div className="w-[92%] ml-24 flex justify-center py-10">
+        <div className="w-[45%] flex flex-col gap-3 justify-center px-5">
+          <h3>Hello there...</h3>
+          <h1 className="text-5xl font-bold">Talha Malik</h1>
+          <p className="text-xl">I Am Passionate</p>
+          <p className="text-md">
+            Enthusiastic Full Stack Developer focused on developing engaging
+            mobile applications that make a positive difference in people's
+            lives using creative concepts. I strive to enhance user experience
+            through effective design and functionality.
+          </p>
+          <div className="flex gap-3">
+            <button className="w-36 h-12 bg-sky-400 text-white">My Work</button>
+            <button className="w-36 h-12 border-2 border-sky-400 text-sky-400">
+              My Work
+            </button>
+          </div>
+        </div>
+        <div className="w-[45%]">
+          <img className="h-[450px]" src={Boy} alt="" />
+        </div>
+      </div>
     </>
   );
 }
